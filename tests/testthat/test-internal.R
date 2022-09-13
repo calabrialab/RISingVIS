c566d96d8b1703f0d557d4882632a183b0b5ffbf
Chiara <106163337+chiarapavia@users.noreply.github.com>
2022-09-13 16:59:08 +0200

    Update functions
    Updated functions for different control cell lines

--- tests/testthat/test-internal.R
+++ tests/testthat/test-internal.R
@@ -5,16 +5,16 @@ library(RISingVIS)
 data(association_file)
 
 ratios_out <- tibble::tribble(
-    ~Sample, ~Ratio,
+    ~Sample, ~CEM37,
     "A", 369.33333333,
-    "B", 221.60000000,
+    "B", 221.60000000, 
     "All samples", 138.50000000
 )
 
 ratios_out_byIS <- tibble::tribble(
-    ~chr, ~integration_locus, ~strand, ~A, ~B, ~`All samples`,
-    "11", 64537168, "-", 154.333333, NA, 154.333333,
-    "2", 73762398, "-", NA, 129, 129
+    ~chr, ~integration_locus, ~strand, ~Control, ~A, ~B, ~`All samples`,
+    "11", 64537168, "-", "CEM37", 154.333333, NA, 154.333333,
+    "2", 73762398, "-", "CEM37", NA, 129, 129
 )
 
 share_mat <- tibble::tribble(
@@ -77,10 +77,54 @@ no_share_others_mat <- tibble::tribble(
     "2", 73762398, "-", "B3", 5,
 )
 
+ctrl_matrix <- tibble::tribble(
+    ~chr, ~integration_locus, ~strand, ~CompleteAmplificationID, ~Value,
+    "11", 64537168, "-", "ctrl1_2", 463,
+    "2", 73762398, "-", "ctrl1_3", 645,
+    "8", 8866486, "+", "ctrl2_1", 703,
+    "11", 64537168, "-", "ctrl2_2", 473,
+    "4", 73292632, "+", "A1", 215,
+    "19", 8621028, "-", "A2", 309,
+    "17", 1632982, "-", "A3", 311,
+    "2", 9876543, "+", "B1", 270,
+    "5", 83626283, "+", "B2", 753,
+    "21", 9243522, "-", "B3", 600,
+    "17", 1632982, "-", "ctrl1_3", 11,
+    "5", 83626283, "+", "ctrl2_1", 7,
+    "11", 64537168, "-", "A2", 3,
+    "2", 73762398, "-", "B3", 5
+)
+
+ctrl_af <- tibble::tibble(ProjectID = "Proj1", PoolID = "Pool1", 
+                          SubjectID = c("ctrl1", "ctrl1", "ctrl1", 
+                                        "ctrl2", "ctrl2", "ctrl2", 
+                                        "A", "A", "A", "B", "B", "B"),
+                          CompleteAmplificationID = c("ctrl1_1", "ctrl1_2", 
+                                                      "ctrl1_3", "ctrl2_1", 
+                                                      "ctrl2_2", "ctrl2_3", 
+                                                      "A1", "A2", "A3", 
+                                                      "B1", "B2", "B3"))
+
+
+ctrl_1_known_is <- tibble::tribble(
+    ~chr,	~integration_locus,	~strand, 
+    "11",	64537168,	"-",
+    "2",	73762398,	"-"
+)
+
+ctrl_2_known_is <- tibble::tribble(
+    ~chr,	~integration_locus,	~strand, 
+    "11",	64537168,	"-",
+    "8",	8866486,	"+"
+)
+
+controls <- list(ctrl1 = ctrl_1_known_is, 
+                 ctrl2 = ctrl_2_known_is)
+
 
 # Test correct output
 
-test_that("find_shared_CEM_IS works correctly", {
+test_that("find_shared_IS works correctly for control IS", {
     mat <- share_mat
     af <- association_file
     agg <- ISAnalytics::aggregate_values_by_key(
@@ -93,7 +137,8 @@ test_that("find_shared_CEM_IS works correctly", {
     colnames(agg)[which(names(agg) == "Value_sum")] <- 
         "value"
     is_vars <- get_is_vars()
-    shared <- find_shared_CEM_IS(agg, is_vars, "SubjectID")
+    shared <- find_shared_IS(agg, is_vars, "SubjectID", "Value", 
+                             ctrl = "CEM37", type = "control")
     shared <- shared %>% 
         dplyr::select(.data$chr, .data$integration_locus, .data$strand) %>% 
         unique()
@@ -105,7 +150,33 @@ test_that("find_shared_CEM_IS works correctly", {
     expect_equal(shared, rows)
 })
 
-test_that("find_shared_other_IS works correctly", {
+test_that("find_shared_IS works correctly for different control", {
+    mat <- ctrl_matrix
+    af <- ctrl_af
+    agg <- ISAnalytics::aggregate_values_by_key(
+        x = mat,
+        association_file = af,
+        value_cols = "Value",
+        key = "SubjectID",
+        group = ISAnalytics::mandatory_IS_vars()
+    )
+    colnames(agg)[which(names(agg) == "Value_sum")] <- 
+        "Value"
+    is_vars <- get_is_vars()
+    shared <- find_shared_IS(agg, is_vars, "SubjectID", "Value", 
+                             controls, "control")
+    shared <- shared[["ctrl1"]] %>% 
+        dplyr::select(.data$chr, .data$integration_locus, .data$strand) %>% 
+        unique()
+    rows <- tibble::tribble(
+        ~chr,	~integration_locus,	~strand,
+        "11",	64537168,	"-",
+        "2",	73762398,	"-"
+    )
+    expect_equal(shared, rows)
+})
+
+test_that("find_shared_IS works correctly for samples IS", {
     mat <- share_mat
     af <- association_file
     agg <- ISAnalytics::aggregate_values_by_key(
@@ -116,9 +187,10 @@ test_that("find_shared_other_IS works correctly", {
         group = ISAnalytics::mandatory_IS_vars()
     )
     colnames(agg)[which(names(agg) == "Value_sum")] <- 
-        "value"
+        "Value"
     is_vars <- get_is_vars()
-    shared <- find_shared_other_IS(agg, is_vars, "SubjectID", "value")
+    shared <- find_shared_IS(agg, is_vars, "SubjectID", "Value",
+                             ctrl = "CEM37", type = "other")
     shared <- shared %>% 
         dplyr::select(.data$chr, .data$integration_locus, .data$strand) %>% 
         unique()
@@ -131,8 +203,6 @@ test_that("find_shared_other_IS works correctly", {
 })
 
 test_that("compute_ratio works correctly", {
-    mat <- share_mat
-    af <- association_file
     shared <- tibble::tribble(
         ~chr, ~integration_locus, ~strand, ~SubjectID, ~value,
         "11", 64537168, "-", "A", 3, 
@@ -141,17 +211,13 @@ test_that("compute_ratio works correctly", {
         "2", 73762398, "-", "CEM37", 645
     )
     is_vars <- get_is_vars()
-    r <- compute_ratio(shared, is_ratio, "SubjectID", "value")
+    r <- compute_ratio(shared, is_vars, "SubjectID", "value", 
+                       "CEM37", "by sample")
     rownames(r) <- NULL
-    output <- ratios_out %>% 
-        dplyr::filter(IS_Source == "CEM") %>% 
-        dplyr::select(Sample, Ratio)
-    expect_equal(tibble::tibble(r), output, tolerance = 0.0001)
+    expect_equal(tibble::tibble(r), ratios_out, tolerance = 0.0001)
 })
 
 test_that("compute_ratio_byIS works correctly", {
-    mat <- share_mat
-    af <- association_file
     shared <- tibble::tribble(
         ~chr, ~integration_locus, ~strand, ~SubjectID, ~value,
         "11", 64537168, "-", "A", 3, 
@@ -160,7 +226,8 @@ test_that("compute_ratio_byIS works correctly", {
         "2", 73762398, "-", "CEM37", 645
     )
     is_vars <- get_is_vars()
-    r <- compute_ratio_byIS(shared, is_vars, "SubjectID", "value")
+    r <- compute_ratio(shared, is_vars, "SubjectID", 
+                            "value", "CEM37", type = "by IS")
     rownames(r) <- NULL
     expect_equal(tibble::tibble(r), ratios_out_byIS, tolerance = 0.0001)
 })
@@ -177,6 +244,20 @@ test_that("files_check works correctly", {
 })
 
 
+# Test ctrl_check
+
+test_that("ctrl_check works correctly", {
+    af <- association_file
+    expect_error(ctrl_check(af, "SubjectID", "Ctrl1"))
+    ctrl <- list("Ctrl1", "Ctrl2", "Ctrl3")
+    expect_error(ctrl_check(af, "SubjectID", ctrl))
+    ctrl <- controls
+    expect_error(ctrl_check(af, "SubjectID", ctrl))
+    af <- ctrl_af
+    expect_true(ctrl_check(af, "SubjectID", ctrl))
+})
+
+
 # Test get_is_vars
 
 test_that("get_is_vars returns three character values", {
@@ -184,3 +265,4 @@ test_that("get_is_vars returns three character values", {
     expect_true(length(is_vars) == 3)
     expect_true(is.character(is_vars))
 })
+
